9c176076e2f4b297e2d336fa5c5d881197b631fa
NemoRock <lexa.brus97@gmail.com>
2022-05-23 12:16:32 +0500

    Hello

--- main.go
+++ main.go
@@ -3,17 +3,5 @@ package main
 import "fmt"
 
 func main() {
-	fmt.Println(Max(1, 4))
-	fmt.Println(Max(5, 4))
-}
-
-func Max(x, y int) int {
-	return Ternary(x > y, x, y)
-}
-
-func Ternary[T any](cond bool, x T, y T) T {
-	if cond {
-		return x
-	}
-	return y
+	fmt.Println("Hello")
 }
